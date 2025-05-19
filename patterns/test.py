381a17084428538ecf5dd6d982cbd6aa1ed41cb5
Asma Tamkeen <asmatamkeen06@gmail.com>
2025-05-19 18:13:52 +0530

    commiting count_digits, armstrong num, palindrome, reverse, and divisors py program to basic maths

--- patterns/test.py
+++
@@ -1,9 +0,0 @@
-def divisors(num):
-    lst=[]
-    for i in range(1,num+1):
-        if num%i==0:
-            lst.append(i)
-            i=i+1
-    return lst
-num=int(input("Enter a number:"))
-print(f"Divisors of {num} are {divisors(num)}")
