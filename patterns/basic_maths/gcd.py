4792194cc8c53e7a729ab3b21a53d44df7d72e1c
Asma Tamkeen <asmatamkeen06@gmail.com>
2025-05-24 21:19:30 +0530

    commiting basic maths programs

--- patterns/basic_maths/gcd.py
+++ patterns/basic_maths/gcd.py
@@ -1,23 +1,26 @@
 def gcd(num1,num2):
-    lst=[]
-    for i in range(1,num1+1):
-        if i%num1==0:
-            lst.append(i)
     lst1=[]
+    for i in range(1,num1+1):
+        if num1%i==0:
+            lst1.append(i)
+    print(lst1)
+    lst2=[]
     for j in range(1,num2+1):
-        if j%num2==0:
-            lst1.append(j)
+        if num2%j==0:
+            lst2.append(j)
+    print(lst2)
     common=[]
-    for k in lst1:
-        for l in lst:
-            if lst1[k]==lst[l]:
-                common.append(k)
+    for i in lst1:
+        if i in lst2:
+                common.append(i)
+    print(common)
 
     gcd=max(common)
     return gcd
 num1=int(input("Enter num1:"))
 num2=int(input("Enter num2:"))
 print(f"GCD of {num1} and {num2} is {gcd(num1,num2)}")
+
 
 
         
