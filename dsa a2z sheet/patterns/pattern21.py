88f1346d00a7eae5c3f03b03b5e736df17573cc2
asmatamkeen <tamkeenasma06@gmail.com>
2026-07-05 14:01:02 +0530

    Complete pattern 21/22

--- dsa a2z sheet/patterns/pattern21.py
+++ dsa a2z sheet/patterns/pattern21.py
@@ -1,5 +1,9 @@
 def pattern21(n):
     for i in range(n):
         if i==0 or i==n-1:
+            print('*'*(n))
+        else:
+            print('*',end='')
+            print(' '*(n-2),end='')
             print('*')
-pattern21(5)
+pattern21(7)
