21b7caa170abd28c65707db9f53e6759fe90e3a6
asmatamkeen <tamkeenasma06@gmail.com>
2026-07-15 22:16:21 +0530

    Second largest element in an array

--- dsa/arrays/second_largest.py
+++ dsa/arrays/second_largest.py
@@ -1,14 +1,16 @@
 def secondLargest(arr):
+    if (len(arr)<2):
+        return None
     first_largest=-1
     second_largest=-1
     for i in range(0,len(arr)):
         if arr[i]>first_largest:
             second_largest=first_largest
             first_largest=arr[i]
-        elif arr[i]>second_largest:
+        elif arr[i]>second_largest and arr[i]!=first_largest:
             second_largest=arr[i]
 
         
     return second_largest
 
-print(secondLargest([4,9,0,2,8,7,1]))
+print(secondLargest([4,9,9,0,2,8,7,1]))
